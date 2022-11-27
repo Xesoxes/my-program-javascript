d4a4de2b09a7d824678942fa7836321238353dbd
SAIfullah <73381115+koreoxy@users.noreply.github.com>
2022-11-27 15:46:51 +0700

    Update console.js

--- console.js
+++ console.js
@@ -1,3 +1,11 @@
 //command line
 console.log('test')
 console.log('sadasdsa')
+<script>
+
+		let counter = 100;
+		do{
+			document.writeln(`<p>Perulangan ke ${counter}</p>`);
+			counter++;
+		}while (counter <= 10);
+</script>
